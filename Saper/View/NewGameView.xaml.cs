e578c86d6c1ad24cdf8779f184e8c756a3f2d9e9
Szymon Pietrzak <szymonpietrzak55@gmail.com>
2025-06-21 19:09:51 +0200

    Options and NewGame Window Prototypes

---
+++ Saper/View/NewGameView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Saper.View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy NewGameView.xaml
+    /// </summary>
+    public partial class NewGameView : Window
+    {
+        public NewGameView()
+        {
+            InitializeComponent();
+        }
+    }
+}
