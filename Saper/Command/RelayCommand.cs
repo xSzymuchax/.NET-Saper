0cbeecbc71bfce2ef7f33e0b89c99921980f9194
Szymon Pietrzak <szymonpietrzak55@gmail.com>
2025-06-21 17:39:57 +0200

    MVVM structure

---
+++ Saper/Command/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Saper.Command
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+        private Action<object> _Execute { get; set; }
+        private Predicate<object> _CanExecute { get; set; }
+
+
+        public RelayCommand(Action<object> Method, Predicate<object> CanExecute)
+        {
+            _Execute = Method ?? throw new ArgumentNullException(nameof(Method));
+            _CanExecute = CanExecute ?? throw new ArgumentNullException(nameof(CanExecute));
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            if (parameter == null)
+                throw new ArgumentNullException(nameof(parameter));
+            return _CanExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (parameter == null)
+                throw new ArgumentNullException(nameof(parameter));
+            _Execute(parameter);
+        }
+    }
+}
