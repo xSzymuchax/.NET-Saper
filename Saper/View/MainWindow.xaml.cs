2922e9272aee2a55b0938568a2aeceae0a1f13d0
Szymon Pietrzak <szymonpietrzak55@gmail.com>
2025-06-21 18:40:49 +0200

    Moved MainWindow to View

--- Saper/View/MainWindow.xaml.cs
+++ Saper/View/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Saper;
+namespace Saper.View;
 
 /// <summary>
 /// Interaction logic for MainWindow.xaml
